ef2b61cd6cc9adb5d3b695c56c4599f1536de4c2
Sohee-Jeun <73260460+Sohee-Jeun@users.noreply.github.com>
2022-01-08 20:46:54 -0800

    js:navbar

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+'use strict';
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+
+document.addEventListener('scroll', () => {
+  
+ if(window.scrollY > navbarHeight){
+     navbar.classList.add('navbar__dark');
+ }else{
+    navbar.classList.remove('navbar__dark')
+ }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) =>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+});
+
+
+
 
 /* Project Imgage Slides*/
 
